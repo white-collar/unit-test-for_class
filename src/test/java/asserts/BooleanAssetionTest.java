d37263b7dce39bff5f95d5c322876c12501d337c
Shmorgun Eugene <eshmorgun@amtoss.com.ua>
2019-08-20 18:11:50 +0300

    Assertions

---
+++ src/test/java/asserts/BooleanAssetionTest.java
@@ -0,0 +1,46 @@
+package asserts;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Write assertions for booleans")
+class BooleanAssertionTest {
+
+    @Nested
+    @DisplayName("When boolean is false")
+    class WhenBooleanIsFalse {
+
+        @Test
+        @DisplayName("Should be false")
+        void shouldBeFalse() {
+            assertFalse(false);
+        }
+
+        @Test
+        @DisplayName("Should be false (with custom error message)")
+        void shouldBeFalseWithCustomErrorMessage() {
+            assertFalse(false, "The boolean is not false");
+        }
+    }
+
+    @Nested
+    @DisplayName("When boolean is true")
+    class WhenBooleanIsTrue {
+
+        @Test
+        @DisplayName("Should be true")
+        void shouldBeTrue() {
+            assertTrue(true);
+        }
+
+        @Test
+        @DisplayName("Should be true (with custom error message)")
+        void shouldTrueWithCustomErrorMessage() {
+            assertTrue(true, "The boolean is not true");
+        }
+    }
+}
